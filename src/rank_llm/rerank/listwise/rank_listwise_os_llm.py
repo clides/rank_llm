e0023f9b0eecf46219828330454c821102bc7e44
clides <daniel168.guo@gmail.com>
2025-05-26 13:32:07 -0400

    fixed chat template

--- src/rank_llm/rerank/listwise/rank_listwise_os_llm.py
+++ src/rank_llm/rerank/listwise/rank_listwise_os_llm.py
@@ -182,8 +182,19 @@ class RankListwiseOSLLM(ListwiseRankLLM):
                     setattr(
                         self._tokenizer,
                         "chat_template",
-                        """{% if not add_generation_prompt is defined %}{% set add_generation_prompt = false %}{% endif %}
-                        {% for message in messages %}{% if not loop.first %}{% endif %}{% if message['role'] == 'system' %}{{ message['content'] + ' ' }}{% elif message['role'] == 'user' %}{{ 'USER: ' + message['content'] + ' ' }}{% elif message['role'] == 'assistant' %}{{ 'ASSISTANT: ' + message['content'] + '</s>' }}{% endif %}{% endfor %}{% if add_generation_prompt %}{{ 'ASSISTANT:' }}{% endif %}""",
+                        """{% if messages[0]['role'] == 'system' %}
+                        {{ 'SYSTEM: ' + messages[0]['content'] + '\n' }}
+                        {% endif %}
+                        {% for message in messages %}
+                        {% if message['role'] == 'user' %}
+                        {{ 'USER: ' + message['content'] + '\n' }}
+                        {% elif message['role'] == 'assistant' %}
+                        {{ 'ASSISTANT: ' + message['content'] + '</s>\n' }}
+                        {% endif %}
+                        {% endfor %}
+                        {% if add_generation_prompt %}
+                        {{ 'ASSISTANT: ' }}
+                        {% endif %}""",
                     )
 
     def rerank_batch(
